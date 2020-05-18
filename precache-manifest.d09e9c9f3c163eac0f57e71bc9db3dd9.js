9a4ee6b1be685ccaa9d2799cce051c04ee2cc8b2
giovannabbottino <gibottino@gmail.com>
2020-05-18 20:17:11 -0300

    Updates

---
+++ precache-manifest.d09e9c9f3c163eac0f57e71bc9db3dd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a45b36b418134fb9ca61e868d39613",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "abc297742858c7bdbe35",
+    "url": "/portfolio/static/css/main.71f6ee75.chunk.css"
+  },
+  {
+    "revision": "38ee6d7d7eeab47bcbcb",
+    "url": "/portfolio/static/js/2.00e0920a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.00e0920a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc297742858c7bdbe35",
+    "url": "/portfolio/static/js/main.d04590bb.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "cfb386bfb8db6f097080fdb69b59b490",
+    "url": "/portfolio/static/media/undraw_logic_n6th.cfb386bf.svg"
+  }
+]);
