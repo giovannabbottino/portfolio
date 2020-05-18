342cf037b7686b0f2782e5789812ed10bf19d98f
giovannabbottino <gibottino@gmail.com>
2020-05-18 16:28:35 -0300

    Updates

---
+++ precache-manifest.34a5a6941a9652ed4cc2b3523533d674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728d1952e41b1c42d14c975a8b88b38d",
+    "url": "/potfolio/index.html"
+  },
+  {
+    "revision": "8548eded875983e7c18b",
+    "url": "/potfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/potfolio/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/potfolio/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8548eded875983e7c18b",
+    "url": "/potfolio/static/js/main.db98d62c.chunk.js"
+  },
+  {
+    "revision": "96a41a1f15afeb9b3824",
+    "url": "/potfolio/static/js/runtime-main.7f7995e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/potfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
