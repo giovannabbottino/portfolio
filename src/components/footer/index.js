4efb6b81f918ba554305bacfc7f053fec2104b1a
giovannabbottino <gibottino@gmail.com>
2020-05-18 23:51:28 -0300

    Adiciona footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import { Fade } from "react-reveal";
+import emoji from "react-easy-emoji";
+
+export default function Footer() {
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className="footer-text">{emoji("Made with ❤️ by Saad Pasta")}</p>
+      </div>
+    </Fade>
+
+  );
+}
---
+++ src/containers/profile/index.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import GithubProfileCard from "../../components/githubProfileCard";
+import axios from "axios";
+import { openSource } from "../../portifolio";
+import Contact from "../../components/contact/";
+
+
+export default function Profile() {
+  const [prof, setrepo] = useState([]);
+  function setProfileFunction(array) {
+    setrepo(array);
+  }
+  function getProfileData() {
+    const client=axios.get("https://api.github.com/users/"+openSource.githubUserName)
+      .then(function(response){
+            setProfileFunction(response.data);
+    });
+  }
+  useEffect(() => {
+    getProfileData();
+  }, []);
+  if (openSource.showGithubProfile === "true"){
+    return ( <GithubProfileCard prof={prof} key={prof.id} /> );
+  } else {
+    return(<Contact />);
+  }
+}
