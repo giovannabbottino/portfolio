3140dd9f9736f79fee0dc4efad095ea2f8e358f2
giovannabbottino <giovannabbottino@gmail.com>
2021-11-23 11:14:36 -0300

    Adiciona portugues como arquivo

---
+++ src/portfolio/portugues.js
@@ -0,0 +1,33 @@
+var buttons={
+    projects: "Projetos",
+    certifications: "Certificações",
+    talks: "Conversas",
+    academic: "Academico",
+    contact: "Entre em contato",
+    resume: "Veja meu CV",
+    more: "MAIS",
+    reach: "Fale comigo!",
+}
+
+const greeting = {
+    /* Your Summary And Greeting Section */
+    username: "Giovanna B Bottino",
+    title: "Hello! How are you?",
+    subTitle: `My name is Giovanna, I am a full-stack web developer and junior data scientist.
+    I develop projects that use Exploratory Analysis, Data Visualization and Machine Learning techniques.
+    I'm also a level 6 artificer in my current D&D campaign.`,
+    resumeLink: "https://drive.google.com/file/d/1jqHyyEXYHJ3GM8Xc-vfEQUeWp5FL5-iJ/view?usp=sharing"
+};
+
+const contactInfo = {
+    title: emoji("Contact Me ☎️"),
+    subtitle: `Discuss a project or just want to say hi? My Inbox is open for all.`,
+    number: "+55 (61) 9 8581-9745",
+    email_address: "giovannabbottino@gmail.com"
+};
+
+export {
+    buttons,
+    greeting,
+    contactInfo,
+};
