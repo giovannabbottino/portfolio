3e88819ca81b14e8fb9fdded02c0a8f8efd87c7d
giovannabbottino <giovannabbottino@gmail.com>
2021-11-23 11:14:55 -0300

    Remove sessoes nao mais requiridas

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,12 +2,9 @@ import React, { Component } from "react";
 
 import Header from "../components/header/";
 import Greeting from "./greeting";
-import Skills from "./skills";
-import Experiences from "./experience";
 import Profile from "./profile";
 import Footer from "../components/footer";
 import Top from "../components/topbutton";
-import EducationSection from "./educationSection";
 
 export default class Main extends Component {
 	render() {
@@ -15,9 +12,6 @@ export default class Main extends Component {
 			<div>
 				<Header />
 				<Greeting />
-				<Skills />
-				<Experiences />
-				<EducationSection />
 				<Profile />
 				<Footer />
 				<Top />
